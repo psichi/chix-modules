144fe118a3e8c4b8ce7be6be63e959072215c279
Rob Halff <rob.halff@gmail.com>
2013-08-31 15:57:38 +0200

    lib updates

--- chix/libs/read.js
+++ chix/libs/read.js
@@ -6,24 +6,62 @@ var res = JSON.parse(fs.readFileSync('packages.json').toString('utf-8'));
 
 var libs = {};
 
+var CdnJsImporter = {};
+
+function categorize(file) {
+
+  var ext = file.split('.').pop();
+  return ext;
+
+}
+
+// devide by javascript, styleheets and other assets.
 res.packages.forEach(function(p) {
 
-  libs[p.name] = { 
-     version: p.version,
-     assets: p.assets
-  }
+  var assets = {}, versions = [];
+  p.assets.forEach(function(a){
+    versions.push(a.version);
+    assets[a.version] = a.files;
+  });
 
+  libs[p.name] = { 
+     name: p.name,
+     description: p.description,
+     homepage: p.homepage,
+     keywords: p.keywords,
+     version: p.version,
+     versions: versions,
+     assets: assets
+  }
 });
 
 function install(name) {
+
   var l;
   if(l = libs[name]) {
 
-    var url = "http://cdnjs.cloudflare.com/ajax/libs/" + l.name + '/' + l.version + '/' + l.filename;
-    console.log(url);
-    http.request(url, function(err, res) {
-      console.log(err, res);
+    var categorized = {};
+    l.assets[l.version].forEach(function(filename, i) {
+
+       var url = "http://cdnjs.cloudflare.com/ajax/libs/" + name + '/' + l.version + '/' + filename;
+       var cat = categorize(url);
+       if(!categorized.hasOwnProperty(cat)) {
+          categorized[cat] = [];
+       }
+
+       categorized[cat].push(url);
+
+      /*
+      http.request(url, function(err, res) {
+        console.log(err, res);
+      });
+      */
+
     });
+
+    l.assets = categorized;
+
+    console.log(l);
 
   } else {
     console.log(name, 'is not available');
@@ -31,4 +69,12 @@ function install(name) {
 
 }
 
-install('twitter-bootstrap');
+// only install/make available what is used and refered to from with the modules.
+// note that sometimes you do not need all javascripts.
+// the user could maybe just select this will creating the instance.
+// maybe just specifiy this, with ask: true in the package.
+// because most of the time you do not need to choose.
+
+//install('twitter-bootstrap');
+//install('jquery');
+install('reveal.js');
