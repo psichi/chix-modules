47e40d1c9f8e85e8e180dd41bb61664d899fbb2c
Rob Halff <rob.halff@gmail.com>
2013-08-31 21:25:55 +0200

    wrong references

--- chix/node/express/rest/module.ts
+++ chix/node/express/rest/module.ts
@@ -1,5 +1,5 @@
-/// <reference path="../node.d.ts" />
-/// <reference path="../express.d.ts" />
+/// <reference path="../../node.d.ts" />
+/// <reference path="../../express.d.ts" />
 
 var mongojs = require("mongojs");
 
