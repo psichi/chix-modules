8a30bedf9dee46233fb0ad8d0521372452fb8c5b
Rob Halff <rob.halff@gmail.com>
2013-08-31 16:03:23 +0200

    create reg

---
+++ createReg.js
@@ -0,0 +1,16 @@
+var glob = require("glob");
+
+// options is optional
+var options = {
+  cwd: 'chix'
+};
+
+glob("**/module.json", options, function (er, files) {
+
+  console.log(files);
+  // files is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // er is an error object or null.
+  
+});
